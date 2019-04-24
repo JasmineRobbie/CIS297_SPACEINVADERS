e58ca0899a81e4facdd6e3bdd3934b54ad06a57a
Jasmine <jrabie@umich.edu>
2019-04-23 23:06:43 -0400

    Beginning
    Remember to install the Win2d nuget package manager. Had some trouble when trying to use visuals with XAML. Right click on the solution explorer and click "restore nuGet packages" and I also right-clicked on the solution in the solution explorer and clicked configuration manager then used the x86 platform and it finally worked for me.

---
+++ SI/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SI
+{
+    
+    public sealed partial class MainPage : Page
+    {
+        Play play;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            play = new Play();
+        }
+
+        private void Canvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            play.DrawGame(args.DrawingSession);
+        }
+    }
+}
---
+++ SI/Play.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace SI
+{
+    public interface IDrawable
+    {
+        void Draw(CanvasDrawingSession canvas);
+    }
+
+    public interface ICollidable
+    {
+        bool Collides(int x, int y, int width, int height);
+    }
+
+    public class Play
+    {
+        private Bullet bullet;
+        private ship PlayerShooter; 
+        private List<IDrawable> drawables;
+
+        public Play()
+        {
+            drawables = new List<IDrawable>();
+            bullet = new Bullet(100, 100, Colors.White);
+            drawables.Add(bullet);
+        }
+
+        public void DrawGame(CanvasDrawingSession canvas)
+        {
+            foreach (var drawable in drawables)
+            {
+                drawable.Draw(canvas);
+            }
+        }
+    }
+
+    public class Bullet : IDrawable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Radius { get; set; }
+        public Color Color { get; set; }
+
+        public Bullet(int x, int y, Color color, int radius = 5)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+            Color = color;
+        }
+
+        public void Draw(CanvasDrawingSession canvas)
+        {
+            canvas.DrawEllipse(X, Y, Radius, Radius, Color, 3);
+        }
+    }
+
+    public class Sprite : IDrawable, ICollidable
+    {
+        public bool Collides(int x, int y, int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(CanvasDrawingSession canvas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+/*
+    public class Wall : IDrawable, ICollidable
+    {
+        public bool Collides(int x, int y, int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(CanvasDrawingSession canvas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    */
+
+
+    public class ship : IDrawable
+    {
+        public void Draw(CanvasDrawingSession canvas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
